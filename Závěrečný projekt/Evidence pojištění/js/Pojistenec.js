f987285cbb5850d17bdf92f1fcbe40571e3f11ff
lenknin <114296945+lenknin@users.noreply.github.com>
2022-10-19 10:47:46 +0200

    Add files via upload

---
+++ Závěrečný projekt/Evidence pojištění/js/Pojistenec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class Pojistenec {
+
+	constructor(jmeno, prijmeni, vek, telefon) {
+		this.jmeno = jmeno;
+		this.prijmeni = prijmeni;
+        this.vek = vek;
+        this.telefon = telefon;
+		
+	}
+
+}
---
+++ Závěrečný projekt/Evidence pojištění/js/obsluha.js
@@ -0,0 +1,3 @@
+'use strict';
+const evidence = new Evidence ();
+evidence.vypisPojistence();
